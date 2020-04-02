9940a3123be18529e9a482cdf52b7b8c169b8915
Adil <adilv022@gmail.com>
2020-04-02 13:12:34 +1100

    Checkout component finished.

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -10,6 +10,7 @@ const Checkout = () => {
             </div>
 
             <div className={s.Steps}>
+
                 <div className={s.FirstItem}>
                     <svg className={`${s.MuiSvgIconRoot} ${s.MuiStepIconRoot} ${s.MuiStepIconActive}`} focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                         <circle cx="12" cy="12" r="12"></circle>
@@ -17,7 +18,9 @@ const Checkout = () => {
                     </svg>
                     <span className={s.Checkout__wayBlock}>Shipping address</span>
                 </div>
-                <span className={`${s.MuiStepConnectorLine} ${s.MuiStepConnectorLineHorizontal}`}></span>
+
+                <div className={`${s.MuiStepConnectorRoot} ${s.MuiStepConnectorHorizontal} ${s.MuiDisabled}`}><span className={`${s.MuiStepConnectorLine} ${s.MuiStepConnectorLineHorizontal}`}></span></div>
+
                 <div className={s.SecondItem}>
                     <svg className={`${s.MuiSvgIconRoot} ${s.MuiStepIconRoot} ${s.MuiStepIconActive}`} focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                         <circle cx="12" cy="12" r="12"></circle>
@@ -25,7 +28,9 @@ const Checkout = () => {
                     </svg>
                     <span className={s.Checkout__wayBlock}>Payment details</span>
                 </div>
-                <span class="MuiStepConnector-line MuiStepConnector-lineHorizontal"></span>
+
+                <div className={`${s.MuiStepConnectorRoot} ${s.MuiStepConnectorHorizontal} ${s.MuiDisabled}`}><span className={`${s.MuiStepConnectorLine} ${s.MuiStepConnectorLineHorizontal}`}></span></div>
+
                 <div className={s.ThirdItem}>
                      <svg className={`${s.MuiSvgIconRoot} ${s.MuiStepIconRoot} ${s.MuiStepIconActive}`} focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                         <circle cx="12" cy="12" r="12"></circle>
@@ -33,6 +38,7 @@ const Checkout = () => {
                     </svg>
                     <span className={s.Checkout__wayBlock}>Review your order</span>
                 </div>
+
             </div>
           </div>
         </div>
